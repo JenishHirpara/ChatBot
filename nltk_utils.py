c4b5f6328b0e37918d79ef7918b0e9a6c63dd5b3
Jenish Hirpara <jenishhirpara23@gmail.com>
2022-04-09 19:48:38 +0530

    tested stemming and tokenizing

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,6 +1,6 @@
 from nltk.stem.porter import PorterStemmer
 import nltk
-nltk.download('punkt')
+# nltk.download('punkt')
 
 stemmer = PorterStemmer()
 
@@ -15,3 +15,13 @@ def stem(word):
 
 def bag_of_words(tokenized_sentence, all_words):
     pass
+
+
+# a = "How long does shipping take?"
+# print(a)
+# a = tokenize(a)
+# print(a)
+
+words = ["organize", "organizes", "organizing"]
+stemmed_words = [stem(w) for w in words]
+print(stemmed_words)
