6e7c50d0102908e2088de312f65d3921d8f1efd8
Jenish Hirpara <jenishhirpara23@gmail.com>
2022-04-09 19:42:23 +0530

    added tokenize and stemming functions

---
+++ nltk_utils.py
@@ -0,0 +1,17 @@
+from nltk.stem.porter import PorterStemmer
+import nltk
+nltk.download('punkt')
+
+stemmer = PorterStemmer()
+
+
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+
+def stem(word):
+    return stemmer.stem(word.lower())
+
+
+def bag_of_words(tokenized_sentence, all_words):
+    pass
